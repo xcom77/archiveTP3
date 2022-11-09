18e9f26f5fd018ec8de03dc672c0be1395f19d0c
xcom77 <113069707+xcom77@users.noreply.github.com>
2022-11-09 16:23:43 +0100

    Update JavaApplication10.java

--- JavaApplication10/src/javaapplication10/JavaApplication10.java
+++ JavaApplication10/src/javaapplication10/JavaApplication10.java
@@ -15,7 +15,7 @@ public class JavaApplication10 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("am");
+       
         System.out.println("am");
         System.out.println("am");System.out.println("am");
     }
